af7c8a6c3c04121cbb64594e4c6cb40d3334ad77
Luis Zambrano <130914970+zambranoluis@users.noreply.github.com>
2024-11-28 00:20:50 -0400

    mnk

--- src/components/Clients.tsx
+++ src/components/Clients.tsx
@@ -11,6 +11,8 @@ import { FaUserCheck } from "react-icons/fa";
 import { FaUserClock } from "react-icons/fa";
 import { FaUserTimes } from "react-icons/fa";
 import { FaUserAltSlash } from "react-icons/fa";
+import { FaSearch } from "react-icons/fa";
+
 
 import { RiCalendarScheduleFill } from "react-icons/ri";
 import { FaUserPen } from "react-icons/fa6";
@@ -22,19 +24,19 @@ import { FaBan } from "react-icons/fa";
 const typeClients = [
   {
     name: "All Users",
-    icon: <FaUserCheck className="text-4xl"/>
+    icon: <FaUserCheck className="text-2xl"/>
   },
   {
     name: "Active",
-    icon: <FaUserClock className="text-4xl"/>
+    icon: <FaUserClock className="text-2xl"/>
   },
   {
     name: "Inactive",
-    icon: <FaUserTimes className="text-4xl"/>
+    icon: <FaUserTimes className="text-2xl"/>
   },
   {
     name: "Banned",
-    icon: <FaUserAltSlash className="text-4xl"/>
+    icon: <FaUserAltSlash className="text-2xl"/>
   },
 ]
 
@@ -43,26 +45,34 @@ const Clients = () => {
 
   return (
     <div className="flex flex-col w-[90%] place-self-center mt-5">
-      <div className="flex bggreen-400 place-self-start ">
-        {
-          typeClients.map((client, index) => (
-            <div
-              className={`select-none ${index === 0 ? "rounded-tl-md" : index === typeClients.length - 1 ? "rounded-tr-md" : ""} flex flex-col items-center justify-center bgpink-500 w-[75px] h-[100px] cursor-pointer border border-b-0 border-[--color-border] ${currentClients === client.name ? "bg-[--color-background-hover] text-[--color-text-hover]" : ""} `}
-              key={client.name}
-              onClick={() => setCurrentClients(client.name)}
-            >
-              {client.icon}
-              {client.name}
-            </div>
-          ))
-        }
+      <div className="flex flex-col bggreen-400 place-self-start ">
+        <div className="flex">
+          {
+            typeClients.map((client, index) => (
+              <div
+                className={`select-none ${index === 0 ? "rounded-tl-md" : index === typeClients.length - 1 ? "rounded-tr-md" : ""} flex flex-col items-center justify-center bgpink-500 w-[75px] h-[80px] cursor-pointer border border-b-0 border-[--color-border] ${currentClients === client.name ? "bg-[--color-background-hover] text-[--color-text-hover]" : ""} text-base `}
+                key={client.name}
+                onClick={() => setCurrentClients(client.name)}
+              >
+                {client.icon}
+                {client.name}
+              </div>
+            ))
+          }
+        </div>
+        <div className="w-[300px] bg-purple-400 flex justify-center items-center rounded-md">
+          <input type="text" className="w-full outline-none bg-[--color-button] text-[--color-background] p-2 rounded-l-md" />
+          <div className="flex p-2 bg-red-600 rounded-r-md ">
+            <FaSearch className="text-2xl" />
+          </div>
+        </div>
       </div>
-      <div className="flex  h-[500px] w-full bgrose-400 place-self-center rounded-b-md rounded-tr-md border border-[--color-border] overflow-y-auto">
+      <div className="flex  h-[500px] w-full bg-[--color-] place-self-center rounded-b-md rounded-tr-md border border-[--color-border] overflow-y-auto">
         <div className="flex flex-col p-4 w-full h-full bgred-300">
         <Card
-          className='w-full max-w-[400px] p-4 shadow shadow-[--color-shadow]'>
+          className='w-full max-w-[400px] p-4 bg-[--color-background] shadow shadow-[--color-shadow]'>
           <CardHeader className='flex gap-[10%]  justify-center  rounded-md P-4'>
-            <Image className='rounded-full w-[100px]' src="/avatarAang.jpg" alt="" />
+            <Image className='rounded-full w-[150px]' src="https://github.com/BPM94/TTMD/raw/main/avatarAang.jpg" alt="" />
             <div className='flex flex-col w-full'>
               <h1>MrMonkaDonk</h1>
               <h1>Seras Victoria Seras Eterna</h1>
